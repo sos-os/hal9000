d90157a4885bcc9dae0e90781f79bd0fc1854c5b
Eliza Weisman <eliza@buoyant.io>
2018-02-04 11:03:37 -0800

    chore: create cargo project

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+//
+// SOS: the Stupid Operating System
+//  by Eliza Weisman (eliza@elizas.website), and the SOS contributors.
+//
+//  Copyright (c) 2018 Eliza Weisman
+//  Released under the terms of the MIT license. See `LICENSE` in the root
+//  directory of this repository for more information.
+//
+//! # HAL-9000: the base SOS Hardware Abstraction Layer
+#![feature(step_trait)]
+#![no_std]
+
+pub mod mem;
+pub mod util;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
