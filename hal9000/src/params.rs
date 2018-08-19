b10a2e89a4941cc30be309cb087253b21b79716d
Eliza Weisman <eliza@buoyant.io>
2018-08-19 10:45:02 -0700

    feat(params): Make mem_map an Iterator

--- hal9000/src/params.rs
+++ hal9000/src/params.rs
@@ -14,6 +14,9 @@ pub trait Architecture {
 pub trait BootParams {
     /// This architecture's physical address type.
     type Arch: Architecture;
+    /// Abstracts over platform-specific details to represent
+    /// the platform's memory map.
+    type MemMap: Iterator<Item = <Self::Arch as Architecture>::MemRegion>;
 
     /// Returns the base address of the kernel memory region.
     fn kernel_base(&self) -> <Self::Arch as Architecture>::PAddr;
@@ -22,7 +25,7 @@ pub trait BootParams {
     fn kernel_end(&self) -> <Self::Arch as Architecture>::PAddr;
 
     /// Returns the memory map.
-    fn mem_map(&self) -> &[<Self::Arch as Architecture>::MemRegion];
+    fn mem_map(&self) -> Self::MemMap;
 
     /// Returns the range of frames containing the kernel binary.
     ///
