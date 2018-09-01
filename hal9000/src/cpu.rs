6d6632388167e246f2518e9df9b46b45e35c93c2
Eliza Weisman <eliza@buoyant.io>
2018-09-01 12:04:27 -0700

    feat(hal::cpu): block out CPU API

--- hal9000/src/cpu.rs
+++ hal9000/src/cpu.rs
@@ -1,11 +1,36 @@
-use ::Architecture;
 use params::BootParams;
+use Architecture;
 
-pub trait Cpu {
+pub trait Cpu: Sized {
     type Arch: Architecture;
+    /// Identifier for a given CPU.
+    type Id;
+    /// Error returned by CPU initialization.
+    type InitError;
 
-    unsafe fn init<B: BootParams<Arch = Self::Arch>>(&P);
+    unsafe fn init<P: BootParams<Arch = Self::Arch>>(
+        params: &P,
+        id: Self::Id,
+    ) -> Result<Self, Self::InitError>;
+}
 
-    unsafe fn enable_irq();
-    unsafe fn disable_irq();
+/// Controls CPU interrupts.
+pub trait IrqCtrl {
+    type Arch: Architecture;
+    /// Error returned by interrupt initialization.
+    type InitError;
+
+    /// Initializes interrupts.
+    unsafe fn init<P>(params: &P) -> Result<(), Self::InitError>
+    where
+        P: BootParams<Arch = Self::Arch>;
+
+    /// Enables interrupts.
+    unsafe fn enable(&mut self);
+
+    /// Disables interrupts.
+    unsafe fn disable_irq(&mut self);
+
+    /// Returns `true` if interrupts are enabled.
+    fn is_enabled(&self) -> bool;
 }
